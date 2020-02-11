43dcc869e06ae68c01f4779ca04e20dd8c2809cb
Dabbeljuh <jwiesemeier@googlemail.com>
2020-02-11 12:53:09 +0000

    changed variables to be more accurate

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-def decimal_to_bin(dec):
+# integer number to binary number
+def integer_to_bin(dec):
   return bin(int(dec))
-
-def decimal_to_hexdec(dec):
+# inteintegerger number to hexainteger number
+def integer_to_hexdec(dec):
   return hex(int(dec))
-
-def bin_to_decimal(binary):
+# binary number to integer number
+def bin_to_integer(binary):
   return int(str(binary), 2)
-
+# binary to hexainteger
 def bin_to_hexdec(binary):
   return hex(int(str(binary), 2))
-
+# hexainteger to binary
 def hex_to_bin(hex):
   return bin(int(hex, 16))
-
-def hex_to_decimal(hex):
+# hexacimal to integer
+def hex_to_integer(hex):
   return int(hex, 16)
-
-def bin_float_to_float(dec):
+# binary with decimal point to decimal 
+def bin_decimal_to_decimal(dec):
   s = str(dec)
   s1, s2 = s.split(".")
   s1 = int(s1, 2)
@@ -28,23 +29,23 @@ def type_recognizer():
   while again == 'y' or again == 'Y':
     print("Please enter number: ")
     print("For binary start with '0b' ")
-    number = input("for hexadecimal start with '0x': \n ")
+    number = input("for hexainteger start with '0x': \n ")
     
     if number[0:2] == '0b':
       if '.' in str(number):
-        print("float: ", bin_float_to_float(number))
+        print("decimal: ", bin_decimal_to_decimal(number))
       else:
-        print('decimal:', bin_to_decimal(number))
+        print('integer:', bin_to_integer(number))
         print('hex:', bin_to_hexdec(number))
     elif number[0:2] == '0x':
-      print('decimal:', hex_to_decimal(number))
+      print('integer:', hex_to_integer(number))
       print('binary:', hex_to_bin(number))
     
     else:
       try:
         float(number)
-        print("binary:", decimal_to_bin(number)) 
-        print("hex: ", decimal_to_hexdec(number))
+        print("binary:", integer_to_bin(number)) 
+        print("hex: ", integer_to_hexdec(number))
       except ValueError:
         print("no valid number")
     again = input('again? y/n: \n' )
